4007f7bfd659cb054151b57fb69360a3c72e272e
KarinaMPalta <karinanmatamalap@gmail.com>
2021-04-28 14:25:46 -0400

    formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
+import { PagesComponent } from './components/pages/pages.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
